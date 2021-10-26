cadb85a26641399653ed58d686a29c60f7e9453f
Willams Silva Andrade <85310845+willamsandrade@users.noreply.github.com>
2021-10-26 08:12:56 -0300

    Add teste 2

--- testes/teste2.php
+++ testes/teste2.php
@@ -1,29 +1,29 @@
 <?php 
- require_once '../DAO/Conexao.class.php';//Anexar arquivo Conexão
- require_once '../entidades/Programador.class.php';//Anexar o arquivo Tipo
- require_once '../DAO/programadorDAO.php';//Anexar o arquivo Tipo
+ require_once '../classes//DAO/Conexao.class.php';//Anexar arquivo Conexão
+ require_once '../classes/entidades/Dev.class.php';//Anexar o arquivo Tipo
+ require_once '../classes/DAO/DevDAO.class.php';//Anexar o arquivo Tipo
 
- $ProgramadorDAO = new ProgramadorDAO(); //Instância do Objeto / Criar Objeto
- $Programador = new Programador(); //Instância do Objeto / Criar Objeto
+ $DevDAO = new DevDAO(); //Instância do Objeto / Criar Objeto
+ $Dev = new Dev(); //Instância do Objeto / Criar Objeto
 
- $Programador->setNome('LucassR'); //Enviando uma informação
- $Programador->setEmail('lcardoso3@gmail.com');
- $Programador->setGithub('lucas_carrdoso'); 
- $Programador->setSenha('12#!52');
- $Programador->setId_linguagem_utilizada(4);   
+ $Dev->setNomeDev('LucassR'); //Enviando uma informação
+ $Dev->setEmailDev('lcardoso3@gmail.com');
+ $Dev->setGithubDev('lucas_carrdoso'); 
+ $Dev->setSenhaDev('12#!52');
+ $Dev->setIdLang(4);   
 
- $ProgramadorDAO->inserirProgramador($Programador);
+ $DevDAO->inserir($Dev);
 
 
- echo "Testes na classe Programador<br>";
+ echo "Testes na classe Dev<br>";
  echo "<hr>";
  echo "Acessando e recuperando dados com os métodos set e get<br>";
 
  echo "Acessando e recuperando dados com o método set e get<br>";
  
- echo"<strong> Nome: </strong>" . $Programador->getNome() . "<br>"; //Recuperando uma informação
- echo"<strong> Email: </strong>" . $Programador->getEmail() . "<br>";
- echo"<strong> Github: </strong>" . $Programador->getGithub() . "<br>";
- echo"<strong> Senha: </strong>" . $Programador->getSenha() . "<br>";
- echo"<strong> Id_linguagem: </strong>" . $Programador->getId_linguagem_utilizada() . "<br>";
+ echo"<strong> Nome: </strong>" . $Dev->getNomeDev() . "<br>"; //Recuperando uma informação
+ echo"<strong> Email: </strong>" . $Dev->getEmailDev() . "<br>";
+ echo"<strong> Github: </strong>" . $Dev->getGithubDev() . "<br>";
+ echo"<strong> Senha: </strong>" . $Dev->getSenhaDev() . "<br>";
+ echo"<strong> Id_linguagem: </strong>" . $Dev->getIdLang() . "<br>";
  ?>
